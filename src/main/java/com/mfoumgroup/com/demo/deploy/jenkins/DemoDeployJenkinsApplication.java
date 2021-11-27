1c985f1317d4ba260daa71f17f0dcf1c579f63cb
Issofa Ntieche Mfoum <issofa.ntieche@gmail.com>
2021-11-27 16:59:26 +0100

    fix

---
+++ src/main/java/com/mfoumgroup/com/demo/deploy/jenkins/DemoDeployJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.mfoumgroup.com.demo.deploy.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeployJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeployJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfoumgroup/com/demo/deploy/jenkins/DemoDeployJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfoumgroup.com.demo.deploy.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeployJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
